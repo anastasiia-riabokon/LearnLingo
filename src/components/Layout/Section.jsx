f106b3828d79944e3aa6ec51d2523a256a563ac4
Anastasiia Riabokon <a.ryabokon20@gmail.com>
2024-10-14 17:32:12 +0300

    styles: fix padding

--- src/components/Layout/Section.jsx
+++ src/components/Layout/Section.jsx
@@ -1,4 +1,4 @@
 const Section = ({children}) => {
-  return <section className="pb-8">{children}</section>;
+  return <section className="pb-8 pt-[20px]">{children}</section>;
 };
 export default Section;
