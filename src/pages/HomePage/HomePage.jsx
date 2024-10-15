d0fbf7aa3da50f8603ef0c3df8dc253a3c2d35ed
Anastasiia Riabokon <a.ryabokon20@gmail.com>
2024-10-15 19:10:10 +0300

    feat: homepage, basic strucrure

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,4 +1,16 @@
+import Info from "../../components/Home/Info";
+import PictureBar from "../../components/Home/PictureBar";
+import Sidebar from "../../components/Home/Sidebar";
+
 const HomePage = () => {
-  return <div>Home</div>;
+  return (
+    <div className="flex flex-col items-center">
+      <div className="flex gap-6 flex-col min-[1440px]:flex-row">
+        <Sidebar />
+        <PictureBar />
+      </div>
+      <Info />
+    </div>
+  );
 };
 export default HomePage;
