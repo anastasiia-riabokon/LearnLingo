817689b64b2a217a641d136c5a0071e187a9b988
Anastasiia Riabokon <a.ryabokon20@gmail.com>
2024-10-16 12:42:47 +0300

    styles: fix color text for dark theme (Info, homepage)

--- src/components/InfoBlock/InfoItem.jsx
+++ src/components/InfoBlock/InfoItem.jsx
@@ -1,8 +1,18 @@
+import {useSelector} from "react-redux";
+import {selectorTheme} from "../../redux/theme/selectors";
+
 const InfoItem = ({item}) => {
+  const theme = useSelector(selectorTheme);
   return (
     <>
       <p className="font-medium leading-[1.14] text-[28px]">{item.qty}</p>
-      <p className="text-[14px] leading-[1.29] text-[rgba(18,20,23,0.7)] w-[74px]">{item.text}</p>
+      <p
+        className={`text-[14px] leading-[1.29] w-[74px] ${
+          theme === "dark" ? "var(--cold-white)" : "text-[rgba(18,20,23,0.7)]"
+        }`}
+      >
+        {item.text}
+      </p>
     </>
   );
 };
